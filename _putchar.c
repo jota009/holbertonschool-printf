7e9e72c0c8ffc76c94c0d3c52e831477bacfad87
josniel <josnielramos@gmail.com>
2025-03-25 21:44:51 -0400

    0-_printf.c single percent fix

--- _putchar.c
+++ _putchar.c
@@ -13,11 +13,3 @@ int _putchar(char c)
 	return write(STDOUT_FILENO, &c, 1);
 }
 
-/**
- * handle_percent - Function that prints percent
- */
-
-int handle_percent(void)
-{
-	return write(STDOUT_FILENO, "%", 1);
-}
